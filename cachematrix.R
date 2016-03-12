a1ff24e8f700e0bb33a4d20fb8901d6fb336a50f
Ammar0347 <k122074@nu.edu.pk>
2016-03-13 00:35:03 +0500

    Modified the code to return inverse
    Added code to both classes.

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,21 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
+library(MASS)
 ## Write a short comment describing this function
-
+##This function is setting and getting the matrix.
+##After that, it calls function to set and get inverse of matrix.
 makeCacheMatrix <- function(x = matrix()) {
-
+	m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
@@ -12,4 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+	m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- ginv(data, ...)
+        x$setinverse(m)
+        m
 }
